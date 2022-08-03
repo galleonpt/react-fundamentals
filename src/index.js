937433535a5f62dc685efb1fd78a62960c919d0f
Zé Luís <jose.rodrigues@peakit.pt>
2022-08-03 19:49:40 +0100

    feat: add development local server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react"
 import ReactDom from "react-dom"
 
 ReactDom.render(
-	<h1 id="title">Olá mundo!123</h1>,
+	<h1 id="title">Olá mundo!!!</h1>,
 	document.getElementById('root')
 )
