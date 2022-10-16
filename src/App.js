d82f074002d1bea6b961c09f09f03e80d9b9e93a
José Rodrigues <jose15.luis00@gmail.com>
2022-10-16 18:31:38 +0100

    working with states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 import Post from "./Posts"
 import Header from "./Header"
 
 export default function App(){
+	const  [posts, setPosts] = useState([
+		{ id: Math.random(), title: "Title#01", subtitle: "Sub#01", likes: 10 },
+		{ id: Math.random(), title: "Title#02", subtitle: "Sub#02", likes: 20 },
+		{ id: Math.random(), title: "Title#03", subtitle: "Sub#03", likes: 30 },
+	])
 
-	const posts = [
-		{ title: "Title#01", subtitle: "Sub#01", likes: 10 },
-		{ title: "Title#02", subtitle: "Sub#02", likes: 20 },
-		{ title: "Title#03", subtitle: "Sub#03", likes: 30 },
-	]
+
+	function handleRefresh(){
+		setPosts((prevState) => [
+			...prevState, 
+			{
+				id: Math.random(), title: `Title#${prevState.length +1}`, subtitle: `Sub#${prevState.length +1}`, likes: 10
+			}
+		])
+	}
 
 	return (
 		<>
 			<Header title="JStack's Blog" subtitle="Weekly posts">
-				<h2>Weekly posts</h2>
+				<h2>
+					Weekly posts
+					<button onClick={handleRefresh}>Refresh</button>
+				</h2>
 			</Header>
 
 			<hr></hr>
