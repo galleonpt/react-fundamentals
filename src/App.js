348412634d4ba41e77539e0a802324efb79a628f
José Rodrigues <jose15.luis00@gmail.com>
2022-10-16 20:57:51 +0100

    context api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {useState} from "react"
-import Post from "./Posts"
+import React, { useState } from "react"
+import Post from "./Post"
 import Header from "./Header"
+import { ThemeProvider } from "./ThemeContext"
+
 
 export default function App(){
+
 	const  [posts, setPosts] = useState([
 		{ id: Math.random(), title: "Title#01", subtitle: "Sub#01", likes: 10, read: true },
 		{ id: Math.random(), title: "Title#02", subtitle: "Sub#02", likes: 20, read: false },
 		{ id: Math.random(), title: "Title#03", subtitle: "Sub#03", likes: 30, read: false },
 	])
+
 
 	function handleRefresh(){
 		setPosts((prevState) => [
@@ -16,15 +20,15 @@ export default function App(){
 				id: Math.random(), title: `Title#${prevState.length +1}`, subtitle: `Sub#${prevState.length +1}`, likes: 10, read: false
 			}
 		])
-	}
+	} 
 
 	function handleRemovePost(postId){
 		setPosts((prevState)=> prevState.filter(post => post.id !== postId))
 	}
 
 	return (
-		<>
-			<Header title="JStack's Blog" subtitle="Weekly posts">
+		<ThemeProvider>
+			<Header title="JStack's Blog">
 				<h2>
 					Weekly posts
 					<button onClick={handleRefresh}>Refresh</button>
@@ -42,6 +46,6 @@ export default function App(){
 					/>
 				)
 			})}
-		</>
+		</ThemeProvider>
 	)
 }
