02aa1c7e458a894fca79b88439365fa846910f79
galleonpt <jose15.luis00@gmail.com>
2022-10-27 23:00:10 +0100

    theming

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Header({ onToggleTheme }) {
+export default function Footer({ onToggleTheme }) {
   return (
     <Container>
-      <h1>JStack's Blog</h1>
+      <span>JStack's Blog. Todos os direitos reservados.</span>
       <button type="button" onClick={onToggleTheme}>🌞</button>
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react"
-import ReactDom from "react-dom"
-import App from "./components/App"
 
-import GlobalStyle from "./styles/global"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDom.render(
-	<>
-		<GlobalStyle />
-		<App />
-	</>,
-	document.getElementById('root')
-)
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
